54b6aed7efcf67c3bc251d210a81bc165b532f54
Paul Hartman <78393505+Paul-Hartman@users.noreply.github.com>
2023-03-07 01:09:57 +0100

    seeds finished

--- seeds/index.js
+++ seeds/index.js
@@ -1,7 +1,10 @@
-const express = require("express");
-const path = require("path")
+
+
 const mongoose = require("mongoose");
-const Campground = require("./models/campground")
+const cities = require("./cities")
+const {places, descriptors} = require("./seedHelpers")
+const Campground = require("../models/campground");
+
 
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
 
@@ -11,3 +14,21 @@ db.once("open", ()=>{
     console.log("database Connected")
 })
 
+const sample = array => array[Math.floor(Math.random() * array.length)]
+
+
+const seedDB = async ()=>{
+    await Campground.deleteMany({});
+    for(let i = 0; i< 50; i++){
+        const random1000 = Math.floor(Math.random() *1000);
+        const camp = new Campground({
+            location : `${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`
+        })
+        await camp.save();
+    }
+}
+
+seedDB().then(() => {
+    db.close();
+})
